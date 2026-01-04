620081d2fa7c1e0d632a6ae1a01e441cc0510304
gembkkim <gembkkim@gmail.com>
2026-01-05 00:16:06 +0900

    ttf delete

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,6 +1,6 @@
 // constants.js (API 기본 URL 설정)
 // Express 서버가 실행 중인 IP 주소와 포트 번호를 사용하세요.
 // Expo 개발 환경에서는 'localhost' 대신 컴퓨터의 로컬 네트워크 IP 주소를 사용해야 합니다.
-const API_BASE_URL = "http://192.168.45.218:3001"; // 예시 IP와 엔드포인트
+const API_BASE_URL = 'http://192.168.45.218:3001'; // 예시 IP와 엔드포인트
 
 export { API_BASE_URL };
